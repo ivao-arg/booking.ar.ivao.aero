d5407b83fdb665d0bb97de493e3f456b1cb172ce
Francisco Gonzalez <78659310+Fransua0206@users.noreply.github.com>
2024-02-22 23:55:36 -0300

    fix(logo): light to dark

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 export const Env = {
     API_HOST: process.env.REACT_APP_API_HOST || 'http://api.booking.ar.ivao.aero',
     LOGO_LIGHT: process.env.REACT_APP_LOGO_LIGHT || 'https://files.ar.ivao.aero/Eventos/kronos_light.svg',
-    LOGO_DARK: process.env.REACT_APP_LOGO_DARK || 'https://files.ar.ivao.aero/Eventos/kronos_light.svg',
+    LOGO_DARK: process.env.REACT_APP_LOGO_DARK || 'https://files.ar.ivao.aero/Eventos/kronos_dark.svg',
     LOGO_SIDEBAR_LIGHT: process.env.REACT_APP_LOGO_SIDEBAR_LIGHT || 'https://files.ar.ivao.aero/PR/symbol_white.svg',
     LOGO_SIDEBAR_DARK: process.env.REACT_APP_LOGO_SIDEBAR_DARK || 'https://files.ar.ivao.aero/PR/symbol_white.svg',
     ANALYTICS_TRACKING_ID: process.env.REACT_APP_ANALYTICS_TRACKING_ID || ''

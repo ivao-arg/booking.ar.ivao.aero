5aa998f2b3a22e4a10c76880bf137f53a7ea40ae
Francisco Gonzalez <78659310+Fransua0206@users.noreply.github.com>
2024-03-09 16:57:47 -0300

    style(events): set image background

--- src/pages/Event/EventDetailsPage.tsx
+++ src/pages/Event/EventDetailsPage.tsx
@@ -93,77 +93,79 @@ export default function EventDetailsPage() {
 
 
     return (
-        <div className="mt-[4.3rem]">
-            <div className="flex flex-col md:flex-row">
-                <div>
-                    <Header textSize="text-xl" textColor="text-blue dark:text-yellow">{event.eventName}</Header>
-                    <Subheader textSize="text-lg" textColor="text-light-blue dark:text-white">{getEventTypeName(event.type)}</Subheader>
+        <div className="w-full h-full min-h-screen bg-[url(https://files.ar.ivao.aero/PR/Images/IVAO_AR_-_Anniversary_2024.jpg)]">
+            <div className="mt-[4.3rem]">
+                <div className="flex flex-col md:flex-row">
+                    <div>
+                        <Header textSize="text-xl" textColor="text-blue dark:text-yellow">{event.eventName}</Header>
+                        <Subheader textSize="text-lg" textColor="text-light-blue dark:text-white">{getEventTypeName(event.type)}</Subheader>
+                    </div>
+                    <div className="md:text-right md:ml-auto text-dark-gray-3 dark:text-light-gray-5">
+                        <span className="block font-header text-[1.1rem] font-extrabold text-blue dark:text-white">
+                            {event.airports.reduce<string[]>((acc, airport) => {
+                                return [...acc, airport.icao];
+                            }, []).join(", ")}
+                        </span>
+                        <span className="block font-header">
+                            {startDate}<br />
+                            {timeRange}
+                        </span>
+                    </div>
                 </div>
-                <div className="md:text-right md:ml-auto text-dark-gray-3 dark:text-light-gray-5">
-                    <span className="block font-header text-[1.1rem] font-extrabold text-blue dark:text-white">
-                        {event.airports.reduce<string[]>((acc, airport) => {
-                            return [...acc, airport.icao];
-                        }, []).join(", ")}
-                    </span>
-                    <span className="block font-header">
-                        {startDate}<br />
-                        {timeRange}
-                    </span>
+
+
+                <div className="flex flex-wrap justify-between mt-7 space-y-4 lg:space-y-0">
+                    <p className="max-w-lg font-action text-justify max-h-80 overflow-y-auto pr-3 pb-1 lg:pb-0 whitespace-pre-line scrollbar-thin scrollbar-thumb-light-gray-5 dark:scrollbar-thumb-dark-gray-5 scrollbar-thumb-rounded">
+                        {event.description}
+                    </p>
+                    <div className="flex flex-col gap-7">
+                        <a href={event.pilotBriefing} target="_blank" rel="noreferrer">
+                            <HorizontalInfoCard
+                                icon={<FiMap size={25} />}
+                                iconBackground="bg-blue"
+                                header={t('info.pilotBriefing.title')}
+                                content={t('info.pilotBriefing.description')} />
+                        </a>
+
+                        <a href={event.atcBriefing} target="_blank" rel="noreferrer">
+                            <HorizontalInfoCard
+                                icon={<FiHeadphones size={25} />}
+                                iconBackground="bg-green"
+                                header={t('info.atcBriefing.title')}
+                                content={t('info.atcBriefing.description')} />
+                        </a>
+                    </div>
                 </div>
-            </div>
 
+                <div className="mt-7">
+                    <Header textSize="text-lg">{t('info.sceneries.title')}</Header>
+                    <Subheader>{t('info.sceneries.description')}</Subheader>
 
-            <div className="flex flex-wrap justify-between mt-7 space-y-4 lg:space-y-0">
-                <p className="max-w-lg font-action text-justify max-h-80 overflow-y-auto pr-3 pb-1 lg:pb-0 whitespace-pre-line scrollbar-thin scrollbar-thumb-light-gray-5 dark:scrollbar-thumb-dark-gray-5 scrollbar-thumb-rounded">
-                    {event.description}
-                </p>
-                <div className="flex flex-col gap-7">
-                    <a href={event.pilotBriefing} target="_blank" rel="noreferrer">
-                        <HorizontalInfoCard
-                            icon={<FiMap size={25} />}
-                            iconBackground="bg-blue"
-                            header={t('info.pilotBriefing.title')}
-                            content={t('info.pilotBriefing.description')} />
-                    </a>
-
-                    <a href={event.atcBriefing} target="_blank" rel="noreferrer">
-                        <HorizontalInfoCard
-                            icon={<FiHeadphones size={25} />}
-                            iconBackground="bg-green"
-                            header={t('info.atcBriefing.title')}
-                            content={t('info.atcBriefing.description')} />
-                    </a>
+                    <div className="flex flex-col md:flex-row gap-7 flex-wrap mt-4">
+                        {Object.entries(eventScenaries).map(([simulator, scenariesByLicence]) => {
+                            const simulatorDescription = `info.sceneries.sims.${simulator.toLowerCase()}.description` as unknown as keyof Translations;
+                            return (
+                                <Fragment key={simulator}>
+                                    <VerticalInfoCard
+                                        header={simulator.toUpperCase()}
+                                        content={t(simulatorDescription)}
+                                    >
+                                        <div className="flex justify-between">
+                                            {scenariesByLicence["freeware"].length > 0 && (
+                                                <DropdownButton text="Freeware">
+                                                    {scenariesByLicence["freeware"].map(scenary => renderScenaryLink(scenary))}
+                                                </DropdownButton>)}
+                                            {scenariesByLicence["payware"].length > 0 &&
+                                                <DropdownButton text="Payware">
+                                                    {scenariesByLicence["payware"].map(scenary => renderScenaryLink(scenary))}
+                                                </DropdownButton>}
+                                        </div>
+                                    </VerticalInfoCard>
+                                </Fragment>)
+                        })}
+                    </div>
                 </div>
-            </div>
-
-            <div className="mt-7">
-                <Header textSize="text-lg">{t('info.sceneries.title')}</Header>
-                <Subheader>{t('info.sceneries.description')}</Subheader>
-
-                <div className="flex flex-col md:flex-row gap-7 flex-wrap mt-4">
-                    {Object.entries(eventScenaries).map(([simulator, scenariesByLicence]) => {
-                        const simulatorDescription = `info.sceneries.sims.${simulator.toLowerCase()}.description` as unknown as keyof Translations;
-                        return (
-                            <Fragment key={simulator}>
-                                <VerticalInfoCard
-                                    header={simulator.toUpperCase()}
-                                    content={t(simulatorDescription)}
-                                >
-                                    <div className="flex justify-between">
-                                        {scenariesByLicence["freeware"].length > 0 && (
-                                            <DropdownButton text="Freeware">
-                                                {scenariesByLicence["freeware"].map(scenary => renderScenaryLink(scenary))}
-                                            </DropdownButton>)}
-                                        {scenariesByLicence["payware"].length > 0 &&
-                                            <DropdownButton text="Payware">
-                                                {scenariesByLicence["payware"].map(scenary => renderScenaryLink(scenary))}
-                                            </DropdownButton>}
-                                    </div>
-                                </VerticalInfoCard>
-                            </Fragment>)
-                    })}
-                </div>
-            </div>
-        </div >
+            </div >
+        </div>
     );
 }
